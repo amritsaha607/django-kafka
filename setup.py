793c20b4f26d0f310c8cff6a021fe9ccb0ec30cc
Amrit Saha <41221083+amritsaha607@users.noreply.github.com>
2024-01-15 19:48:31 +0530

    Introduce Setup.py (#1)
    * added setup.py
    
    * updated license and ignore
    
    * added workflow
    
    * added hello method & fixed setup.py package find location

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='django-kafka',
+    version='0.0.4',
+    description='An utility library for django & kafka integration',
+    package_dir={
+        '': 'app',
+    },
+    packages=find_packages(where='app'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/amritsaha607/django-kafka.git',
+    author='amritsaha607',
+    author_email='amritsaha607@gmail.com',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)
