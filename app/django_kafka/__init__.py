8573f371f2fd9a66c3e5b1cdc37e9d760f6b9979
Amrit Saha <amritsaha607@gmail.com>
2024-02-07 10:28:06 +0530

    added kafka producer and consumer config & listener decorator

--- app/django_kafka/__init__.py
+++ app/django_kafka/__init__.py
@@ -1,1 +1,2 @@
-from .src.hello import hello
+from .consumer import get_kafka_consumer_from_config, kafka_listener
+from .producer import get_kafka_producer_from_config
---
+++ app/django_kafka/consumer/__init__.py
@@ -0,0 +1,2 @@
+from .consumer import get_kafka_consumer_from_config
+from .decorators import kafka_listener
